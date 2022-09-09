f8343c81c8125b700306e9a7de1b7e7908ef4a88
Miss-Bentil <amabbentil@gmail.com>
2022-09-09 19:07:37 +0000

    odd numers less then 100

---
+++ 01-odd-numbers-less-than-100/index1.js
@@ -0,0 +1,13 @@
+
+// initializing it @ 1 +2 results to an odd number eg... 1+2=3; 3+2=5; 5+2=7
+for(let i=1; i<=100; i+=2){
+    console.log(i);
+}
+
+/************************OR********************/
+
+for(let i=0; i<=100; i++){
+    if(i%2 !=0){
+        console.log(i);
+    }
+}
